934c5411457fecdb3f2a2101aba1608c8c1d4783
itsonlyKareem <itsonlykareem@gmail.com>
2022-03-06 21:31:15 +0200

    News Page start

--- app/src/main/java/com/app/helloar/MainActivity.java
+++ app/src/main/java/com/app/helloar/MainActivity.java
@@ -2,11 +2,17 @@ package com.app.helloar;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.view.WindowManager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
@@ -18,11 +24,40 @@ import com.google.ar.sceneform.ux.TransformableNode;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        getViews();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentsContainer,new HomeFragment()).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.homeFragment:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.newsFragment:
+                        fragment = new NewsFragment();
+                        break;
+                    case R.id.diagnosisFragment:
+                        fragment = new DiagnosisFragment();
+                        break;
+                    case R.id.organVisFragment:
+                        fragment = new OrganVisFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentsContainer,fragment).commit();
+                return true;
+            }
+        });
 
 
         //Test commit with yehya
@@ -46,6 +81,10 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
+    }
+
+    private void getViews() {
+        bottomNavigationView = findViewById(R.id.bottomNavView);
     }
 
 //    private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
