605206c56dcc67fdc25025cdb390879a1460332a
yahya Elfeki <yahyaemad37@gmail.com>
2022-03-08 12:24:54 +0200

    yehya's latest work

---
+++ app/src/main/java/com/app/helloar/SelectedNews.java
@@ -0,0 +1,14 @@
+package com.app.helloar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectedNews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_news);
+    }
+}
